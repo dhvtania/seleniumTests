b43bc32266bca9d175e1e4cca9d70f17bd9af06d
nastenkomisha <nastenkomisha@gmail.com>
2014-01-13 19:57:18 +0400

    Some refactoring

--- src/test/java/seleniumTests/pages/LoginPageTest.java
+++ src/test/java/seleniumTests/pages/LoginPageTest.java
@@ -16,8 +16,7 @@ public class LoginPageTest extends LoginPageTestBase  {
     private final CharSequence wrongRegExpNickNameSymbol = "\\asdf";
     private final CharSequence password = "test";
     private final CharSequence unusedNickName = "unusedNickName";
-    private final CharSequence wrongAgainPassword = "t";
-
+    private final CharSequence wrongPasswordAgain = "t";
 
     @BeforeClass
     public void testInit() {
@@ -34,31 +33,12 @@ public class LoginPageTest extends LoginPageTestBase  {
         //click "Вход" button
         clickLogin();
 
-        //sent wrongRegExpNickName captcha is dysplayed
-        enterNickName(wrongRegExpNickNameShort);
-
-        //check that loginInfo is dysplayed
-        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
-        Assert.assertEquals("Wrong LoginInfoText","Никнейм должен удовлетворять регэкспу: \\w{3,15}",loginPage.getLoginInfo().getText());
-
-        //check that captcha is dysplayed
-        Assert.assertTrue(loginPage.getLoginCaptcha().isDisplayed());
-
-        //check that password and againPassword fields have correct color
-        Assert.assertEquals("Wrong password field color","rgba(255, 255, 255, 1)", loginPage.getPassword().getCssValue("background-color"));
-        Assert.assertEquals("Wrong passwordAgain field color","rgba(255, 255, 255, 1)",loginPage.getPasswordAgain().getCssValue("background-color"));
+        enterWrongRegExpNickName(wrongRegExpNickNameShort);
 
         //sent password to password field
         enterPassword(password);
 
-        //check that againPassword field has correct color
-        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
-
-        //sent password to againPassword field
-        enterPasswordAgain(password);
-
-        //check that againPassword field has correct color
-        Assert.assertEquals("Wrong passwordAgain field color", "rgba(255, 255, 255, 1)", loginPage.getPasswordAgain().getCssValue("background-color"));
+        enterCorrectAgainPassword(password);
 
         //check that registration button is disaibled
         Assert.assertFalse("Registration button should be disabled",loginPage.getSubmitButton().isEnabled());
@@ -70,28 +50,27 @@ public class LoginPageTest extends LoginPageTestBase  {
     public void registrationWithWrongPasswordAgain(){
         clickLogin();
 
-        enterNickName(unusedNickName);
-        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
-        Assert.assertEquals("Wrong LoginInfoText","Приятно видеть новых людей!\n" +
-                "Этот никнейм еще не занят.",loginPage.getLoginInfo().getText());
+        enterCorrectUnusedNickName(unusedNickName);
+        
         enterPassword(password);
-        enterPasswordAgain(wrongAgainPassword);
-        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
+        
+        enterWrongPasswordAgain(wrongPasswordAgain);
+
         Assert.assertFalse("Registration button should be disabled",loginPage.getSubmitButton().isEnabled());
 
         refreshPage();
     }
 
     @Test
-    public void registrationWithWrongNickNameAndPassword(){
+    public void registrationWithWrongNickNameAndPasswordAgain(){
         clickLogin();
 
-        enterNickName(wrongRegExpNickNameSymbol);
-        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
-        Assert.assertEquals("Wrong LoginInfoText","Никнейм должен удовлетворять регэкспу: \\w{3,15}",loginPage.getLoginInfo().getText());
+        enterWrongRegExpNickName(wrongRegExpNickNameSymbol);
+
         enterPassword(password);
-        enterPasswordAgain(wrongAgainPassword);
-        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
+
+        enterWrongPasswordAgain(wrongPasswordAgain);
+
         Assert.assertFalse("Registration button should be disabled",loginPage.getSubmitButton().isEnabled());
 
         refreshPage();
@@ -100,51 +79,51 @@ public class LoginPageTest extends LoginPageTestBase  {
     @Test
     public void registrationWithoutClosingRegistrationForm() throws InterruptedException {
         clickLogin();
+
+        enterWrongRegExpNickName(wrongRegExpNickNameShort);
         //sent wrongRegExpNickName captcha is dysplayed
-        enterNickName(wrongRegExpNickNameShort);
-
-        //check that the loginInfo is dysplayed
-        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
-        Assert.assertEquals("Wrong LoginInfoText","Никнейм должен удовлетворять регэкспу: \\w{3,15}",loginPage.getLoginInfo().getText());
-
-        //check that the captcha is dysplayed
-        Assert.assertTrue(loginPage.getLoginCaptcha().isDisplayed());
-
-        //check that password and againPassword fields have correct color
-        Assert.assertEquals("Wrong password field color","rgba(255, 255, 255, 1)", loginPage.getPassword().getCssValue("background-color"));
-        Assert.assertEquals("Wrong passwordAgain field color","rgba(255, 255, 255, 1)",loginPage.getPasswordAgain().getCssValue("background-color"));
 
         //sent password to the password field
         enterPassword(password);
 
-        //check that the againPassword field has correct color
-        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
-
         //sent password to the againPassword field
-        enterPasswordAgain(password);
-
-        //check that the againPassword field has correct color
-        Assert.assertEquals("Wrong passwordAgain field color", "rgba(255, 255, 255, 1)", loginPage.getPasswordAgain().getCssValue("background-color"));
+        enterCorrectAgainPassword(password);
 
         //check that registration button is disaibled
         Assert.assertFalse("Registration button should be disabled",loginPage.getSubmitButton().isEnabled());
 
-        enterNickName(unusedNickName);
-        wait.until(ExpectedConditions.textToBePresentInElement(loginPage.getLoginInfo(),"Приятно видеть новых "));
-        enterPassword(password);
-        enterPasswordAgain(wrongAgainPassword);
-        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
-        Assert.assertFalse("Registration button should be disabled",loginPage.getSubmitButton().isEnabled());
+        clearLoginPage();
 
-        enterNickName(wrongRegExpNickNameSymbol);
-        wait.until(ExpectedConditions.textToBePresentInElement(loginPage.getLoginInfo(), "Никнейм должен удовлетворять регэкспу: \\w{3,15}"));
+        enterCorrectUnusedNickName(unusedNickName);
+
         enterPassword(password);
-        enterPasswordAgain(wrongAgainPassword);
-        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
+
+        enterWrongPasswordAgain(wrongPasswordAgain);
+
+        Assert.assertFalse("Registration button should be disabled", loginPage.getSubmitButton().isEnabled());
+
+        clearLoginPage();
+
+        //Can't use enterWrongRegExpNickName
+        enterNickName(wrongRegExpNickNameSymbol);
+        //check that loginInfo is dysplayed
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
+        wait.until(ExpectedConditions.textToBePresentInElement(loginPage.getLoginInfo(), "Никнейм должен удовлетворять регэкспу: \\w{3,15}"));
+        //check that captcha is dysplayed
+        Assert.assertTrue(loginPage.getLoginCaptcha().isDisplayed());
+        //check that password and againPassword fields have correct color
+        Assert.assertEquals("Wrong password field color","rgba(255, 255, 255, 1)", loginPage.getPassword().getCssValue("background-color"));
+        Assert.assertEquals("Wrong passwordAgain field color","rgba(255, 28, 8, 0.317647)" ,loginPage.getPasswordAgain().getCssValue("background-color"));
+
+        enterPassword(password);
+
+        enterWrongPasswordAgain(wrongPasswordAgain);
+
         Assert.assertFalse("Registration button should be disabled",loginPage.getSubmitButton().isEnabled());
 
         refreshPage();
     }
+
 
     private void clickLogin() {
         mainPage.getLoginButton().click();
@@ -161,6 +140,8 @@ public class LoginPageTest extends LoginPageTestBase  {
         loginPage.getPassword().click();
         loginPage.getNickName().clear();
         loginPage.getPassword().sendKeys(password);
+        //check that againPassword field has correct color
+        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
     }
 
     private void enterNickName(CharSequence nickName) {
@@ -168,6 +149,61 @@ public class LoginPageTest extends LoginPageTestBase  {
         loginPage.getNickName().clear();
         loginPage.getNickName().sendKeys(nickName);
     }
+
+    private void enterCorrectUnusedNickName(CharSequence nickName) {
+        enterNickName(nickName);
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
+        wait.until(ExpectedConditions.textToBePresentInElement(loginPage.getLoginInfo(),"Приятно видеть новых людей!\n" +
+                "Этот никнейм еще не занят."));
+
+        //check that captcha is dysplayed
+        Assert.assertTrue(loginPage.getLoginCaptcha().isDisplayed());
+
+        //check that password and againPassword fields have correct color
+        Assert.assertEquals("Wrong password field color","rgba(255, 255, 255, 1)", loginPage.getPassword().getCssValue("background-color"));
+        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
+    }
+
+    private void enterWrongRegExpNickName(CharSequence wrongNickName) {
+        //sent wrongRegExpNickName captcha is dysplayed
+        enterNickName(wrongNickName);
+
+        //check that loginInfo is dysplayed
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getLoginInfo()));
+        wait.until(ExpectedConditions.textToBePresentInElement(loginPage.getLoginInfo(), "Никнейм должен удовлетворять регэкспу: \\w{3,15}"));
+
+        //check that captcha is dysplayed
+        Assert.assertTrue(loginPage.getLoginCaptcha().isDisplayed());
+
+        //check that password and againPassword fields have correct color
+        Assert.assertEquals("Wrong password field color","rgba(255, 255, 255, 1)", loginPage.getPassword().getCssValue("background-color"));
+        Assert.assertEquals("Wrong passwordAgain field color","rgba(255, 255, 255, 1)",loginPage.getPasswordAgain().getCssValue("background-color"));
+    }
+
+    private void enterCorrectAgainPassword(CharSequence password) {
+        //sent password to againPassword field
+        enterPasswordAgain(password);
+
+        //check that againPassword field has correct color
+        Assert.assertEquals("Wrong passwordAgain field color", "rgba(255, 255, 255, 1)", loginPage.getPasswordAgain().getCssValue("background-color"));
+    }
+
+    private void enterWrongPasswordAgain(CharSequence wrongPasswordAgain) {
+        enterPasswordAgain(wrongPasswordAgain);
+
+        Assert.assertEquals("Wrong password again color", "rgba(255, 28, 8, 0.317647)", loginPage.getPasswordAgain().getCssValue("background-color"));
+    }
+
+    private void clearLoginPage() {
+        loginPage.getNickName().clear();
+        loginPage.getPassword().clear();
+        loginPage.getPasswordAgain().clear();
+        //loginPage.getLoginCaptcha().clear();
+    }
+
+
+
+
 
 
 }
